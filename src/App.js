b577318b0480d5017b2bd8f0106ac80dd9c907c8
llalit <erlalit.lahoria@gmail.com>
2020-09-26 19:47:03 +0530

    netflix clone v1.0 by lahoria

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import requsets from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* Nav */}
+      <Nav />
+      {/* Banner */}
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requsets.fetchTreanding} />
+      <Row title="Top Rated" fetchUrl={requsets.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requsets.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requsets.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requsets.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requsets.fetchRomanceMovies} />
+      <Row title="Documentry Movies" fetchUrl={requsets.fetchDocumentaries} />
     </div>
   );
 }
