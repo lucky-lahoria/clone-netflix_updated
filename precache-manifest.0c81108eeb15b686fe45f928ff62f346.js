ac993ac44acbee2a607aafced9d197554bbd41d5
oci <erlalit.lahoria@gmail.com>
2021-03-05 16:55:41 +0530

    Updates

---
+++ precache-manifest.0c81108eeb15b686fe45f928ff62f346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2b89e6f86960737d03e5b84b166d68",
+    "url": "/clone-netflix/index.html"
+  },
+  {
+    "revision": "44ae865501bab54cbff2",
+    "url": "/clone-netflix/static/css/main.c64b0269.chunk.css"
+  },
+  {
+    "revision": "ccbcf9c1301f55915634",
+    "url": "/clone-netflix/static/js/2.2cfb30a4.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/clone-netflix/static/js/2.2cfb30a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ae865501bab54cbff2",
+    "url": "/clone-netflix/static/js/main.a273f2ec.chunk.js"
+  },
+  {
+    "revision": "959c2486cb1511e0d39c",
+    "url": "/clone-netflix/static/js/runtime-main.73cee1ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clone-netflix/static/media/logo.5d5d9eef.svg"
+  }
+]);
